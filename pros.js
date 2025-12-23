54a775f0c55c543ea5dc76e4248999255cf98dda
asad-fleek <asad.mahmood@pendoah.ai>
2025-12-24 02:01:18 +0500

    projectcompleted

---
+++ pros.js
@@ -0,0 +1,10 @@
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+    console.log(123);
+    
+}
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
